7f5ea297cafde40a57b4f37337122ecf3cae4645
Pablo Palacios <25efilm.infobibliotecas@gmail.com>
2022-12-22 12:59:16 -0500

    Init

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Pokedex',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Pokedex'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
